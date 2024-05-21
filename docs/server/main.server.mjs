c79a7f29c43eb779a7d51d79b9e2f838e0c1736a
Temurbie <Temurbie@gmail.com>
2024-05-21 23:02:29 +0400

    add

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CY4DMHZG.mjs";import"./chunk-6YQYIHQG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PGA6JFYE.mjs";import"./chunk-6YQYIHQG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
